421cb7ff649415c6c0e31bdccf0908e3a656bda6
EsniMedia <riquelmetomas666@gmail.com>
2026-02-21 12:23:03 -0300

    inicio completo

---
+++ js/contacto-button.js
@@ -0,0 +1,8 @@
+
+const botoncontacto = document.getElementById("boton-contacto")
+
+//funcion
+botoncontacto.addEventListener('click', function(){
+    window.location.href = "../pages/contacto.html"
+
+})
