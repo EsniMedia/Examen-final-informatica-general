60243c9a04d8b57deb679c6afcb7b636df389914
EsniMedia <riquelmetomas666@gmail.com>
2026-03-01 13:55:48 -0300

    detalles

--- js/galerias.js
+++ js/galerias.js
@@ -1,7 +1,7 @@
 // Seleccionamos todos los carruseles que tengan data-region
 document.querySelectorAll('.carrusel-individual').forEach(cuadro => {
     cuadro.addEventListener('click', (e) => {
-        // Si el click no fue en un control...
+        // Si el click no fue en un control
         if (!e.target.closest('.carousel-control-next') && !e.target.closest('.carousel-control-prev')) {
             // Obtenemos la región (caba, jujuy, etc) y armamos la URL automáticamente
             const provincia = cuadro.getAttribute('data-provincia').toLowerCase();
